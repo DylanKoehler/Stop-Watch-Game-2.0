949577d44467eb90e95b6c7543229a2372334681
Dylan Koehler <Dylankoehler3212@gmail.com>
2021-11-10 10:35:55 -0600

    Initial Commit

---
+++ Stop Watch Game 2.0/Stop_Watch_Game_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_Watch_Game_2_0App.swift
+//  Stop Watch Game 2.0
+//
+//  Created by Student on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Stop_Watch_Game_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
